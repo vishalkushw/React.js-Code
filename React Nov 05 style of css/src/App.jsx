913ff8038bba7d7b21dec584a2fe41ced9c4d577
vishal kushwah <kushwahvishal2391@gmail.com>
2024-12-25 09:33:26 +0530

    React Nov 05 style of css

---
+++ React Nov 05 style of css/src/App.jsx
@@ -0,0 +1,75 @@
+
+// const App=()=>{
+//   return(
+//      <>   
+//       <h1 style={{color:"red",fontFamily:"verdana"}}>wel come to Bhopal</h1>
+//       <h2 style={{color:"blue",fontFamily:"sans-serif",}}>Inline css</h2>
+//       <div>
+//         <div style={{width:300,height:300,border:"2px solid black",borderRadius:"5px",backgroundColor:"yellow",justifyContent:"center",alignItems:"center",textAlign:"center"}}>
+//           <div style={{width:200,height:150,border:"2px solid black",borderRadius:"5px",backgroundColor:"skyblue"}}>
+//             <div style={{color:"red", width:100,height:50,border:"2px solid black",borderRadius:"5px",backgroundColor:"green"}}>cybrom</div>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+
+//  export default App;
+
+//   const data={
+//    color:"red",
+//    fontFamily:"verdana",
+//    textDecoration:"underline"
+//  }
+
+// const mydata={
+//   color:"blue",
+//   fontFamily:"arial",
+//   textDecoration:"overline",
+//   backgroundColor:"yellow"
+
+// }
+
+
+// const App=()=>{
+//   return(
+//      <>   
+//       <h1 style={data}>Wel come to Bhopal</h1>
+//       <h2 style={mydata}>My name is vishal</h2>
+//     </>
+//   )
+// }
+
+// export default App;
+
+// import Cybrom from "./Cybrom"
+// const App=()=>{
+//   return(
+//     <>
+//    <h1>Hello my name is vishal kushwah</h1> 
+//    <h2>We are mern Developer </h2>
+//    <Cybrom/>
+//     </>
+//   )
+// }
+// export default App
+
+
+const App=()=>{
+  return(
+    <>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Data</li>
+      <li>Contact</li>
+    </ul>
+
+    <h1>Vishal kushwah</h1>
+
+
+    </>
+  )
+}
+ export default App
---
+++ React Nov 05 style of css/src/Cybrom.jsx
@@ -0,0 +1,11 @@
+
+const Cybrom=()=>{
+    return(
+       <>   
+        <h1>We are cybrom student bhopal</h1>
+        <h2>Hii vishal</h2>
+      </>
+    )
+  }
+  
+  export default Cybrom;
