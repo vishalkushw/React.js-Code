745bd51734eb5298d4fa8446d00e407f94e1e502
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-01 14:42:24 +0530

    React dec 09  redux colors

---
+++ React Dec 09 redux colors/src/App.jsx
@@ -0,0 +1,27 @@
+import { useSelector,useDispatch } from "react-redux";
+import { changeColor } from "./colorSlice";
+import { useState } from "react";
+
+const App=()=>{
+   const color=useSelector((state)=>state.mycolor.clr);
+   const[mycolor,setmyColor]=useState("");
+
+   const dispatch= useDispatch();
+   const setColor=()=>{
+      dispatch(changeColor({myclr:mycolor}))
+   }
+   return(
+      <> 
+      <h1>Change color</h1>
+      enter color:<input type="text" value={mycolor}
+      onChange={(e)=>{setmyColor(e.target.value)}}/>
+      <button onClick={setColor}>Click here</button>
+      <br /><br />
+      <div style={{width:"300px",height:"300px",backgroundColor:color,borderRadius:"50%", border:"2px solid black"}}>
+
+        <h1 style={{textAlign:"center",paddingTop:90}}>Vishal kushwah</h1>
+   </div> 
+      </>
+   )
+}
+export default App; 
