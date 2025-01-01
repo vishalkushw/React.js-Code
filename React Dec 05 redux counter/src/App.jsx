79cdeabcb0209d2ab5c29bcf6e15e3eed3a3ea45
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-01 14:15:21 +0530

    React dec 05 redux counter

---
+++ React Dec 05 redux counter/src/App.jsx
@@ -0,0 +1,13 @@
+import Cybrom from './Cybrom';
+
+const App=()=>{
+  return(
+    <>
+    <h1 style={{textAlign:"center", paddingTop:"40px", color:"red"}} >Reduce Counter Program</h1>
+    <br />
+    <Cybrom/>
+    </>
+  )
+}
+
+export default App;
---
+++ React Dec 05 redux counter/src/Cybrom.jsx
@@ -0,0 +1,19 @@
+import {increment, decrement} from "./counterSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+const Cybrom=()=>{
+    const myval=useSelector(state=>state.mycounter.count);
+        const dispatch=useDispatch();
+    return(
+        
+        <>
+        <div style={{textAlign:"center"}}>
+        <h1> Welcome to counter program</h1>
+        <button onClick={()=>{dispatch(increment())}}>Increment</button>
+        <h1>{myval}</h1>
+        <button onClick={()=>{dispatch(decrement())}}>decrement</button>
+        </div>
+        </>
+    )
+}
+export default Cybrom;
