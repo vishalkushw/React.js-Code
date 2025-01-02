772ddbe22e3bd175318d5b2c28ef6425f51e4db3
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-02 09:08:15 +0530

    React dec 11 redux todoSlice 2

---
+++ React Dec 11 redux todoSlice 2/src/App.jsx
@@ -0,0 +1,73 @@
+import { useSelector,useDispatch } from "react-redux";
+import { addTask, deleteTask, editDataSave } from "./todoSlice";
+import { useState } from "react";
+
+const App=()=>{
+   const Data=useSelector((state)=>state.todo.task);
+   const dispatch=useDispatch();
+const [val,setVal]=useState("");
+
+const [btnStatus,setBtnStatus]=useState(true);
+const [newid,setNewid]= useState("");
+console.log(Data);
+
+const handleSubmit=()=>{
+   dispatch(addTask({ id:Date.now() ,work:val}))
+
+}
+const recDel=(id)=>{
+   dispatch(deleteTask({id:id}))
+}
+const editTask=(id,work)=>{
+   setBtnStatus(false);
+   setVal(work)
+   setNewid(id)
+}
+const editSave=()=>{
+   dispatch(editDataSave({id:newid,work:val}));
+   setBtnStatus(true);
+}
+let sn=0;
+const ans=Data.map((key)=>{
+   sn++;
+   return(
+      <>
+      <tr>
+         <td>{sn}</td>
+         <td>{key.work}</td>
+         <td><button onClick={()=>{recDel(key.id)}}>Delete</button></td>
+         <td><button onClick={()=>{editTask(key.id,key.work)}}>Edit</button></td>
+         
+      </tr>
+      
+      </>
+   )
+})
+   return(
+      <div style={{backgroundColor:"yellow"}}>
+      <>
+      
+      <h1>TO Do App
+
+      </h1>
+
+      Enet Task:<input type="taxt" value={val} onChange={(e)=>{setVal(e.target.value)}}/>
+      
+    {btnStatus?  <button onClick={handleSubmit}>Add</button>:
+    <button onClick={()=>{editSave()}}>Edit Save</button>}
+      <hr size="1"  color="red"/>
+      <table>
+         <tr>
+            <th>Sno</th>
+            <th>Work</th>
+            <th></th>
+         </tr>
+         {ans}
+      </table>
+      
+      
+      </>
+      </div>
+   )
+}
+export default App;
