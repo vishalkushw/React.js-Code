f67a4357758843271bf504bf9cf6d749a2edb3b3
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-02 09:10:07 +0530

    React dec 12.1 redux todoSlice 4

---
+++ React Dec 12.1 redux todoSlice 4/src/todoSlice.jsx
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+const todoSlice= createSlice({
+    name:"todo",
+    initialState:{
+        task:[]
+    },
+    reducers:{        
+        addTask:(state, actions)=>{
+          state.task.push(actions.payload);
+        },
+        deleteTask:(state, actions)=>{
+        console.log(actions.payload.id);
+        state.task=state.task.filter(key=> key.id!=actions.payload.id);
+        },
+        editDataSave:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].work=actions.payload.work;
+                }
+            }
+        },
+
+        completeTask:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].status="complete";
+                }
+            }
+        },
+
+        uncompleteTask:(state, actions)=>{
+            for (var i=0; i<state.task.length; i++)
+            {
+                if (state.task[i].id==actions.payload.id)
+                {
+                    state.task[i].status="uncomplete";
+                }
+            }
+        }
+    }
+})
+export const {addTask, deleteTask, editDataSave, completeTask, uncompleteTask} = todoSlice.actions;
+export default todoSlice.reducer;
