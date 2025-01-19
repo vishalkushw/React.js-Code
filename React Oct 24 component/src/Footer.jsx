1c4e51caeb5c47ac8a85414940ed2267b272db89
vishal kushwah <kushwahvishal2391@gmail.com>
2025-01-19 12:38:19 +0530

    React Oct 24 component

--- React Oct 24 component/src/Footer.jsx
+++ React Oct 24 component/src/Footer.jsx
@@ -3,7 +3,10 @@ const Footer=()=>{
     return(
        <>   
        <h1>This is my footer information </h1>
+      
        <Copyinfo/>
+
+       <h1>  Create by vishal kushwah</h1>
       </>
     )
   }
